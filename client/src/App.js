93a8b5e56b21ca65fdd8e3881e487a32b6c0df76
Lalit <lalit@Lalits-MacBook-Pro.local>
2025-08-18 21:31:22 +0530

    Adding Top Hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./sections/Hero";
 import About from "./sections/About";
 
 export default function App() {
 
   return (
     <div>
+      <Hero />
       <About />
     </div>
   );
---
+++ client/src/sections/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+
+  return (
+    <section id="home" className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">Lalit Sharma</h1>
+        <h2 className="hero-subtitle">Full Stack Developer</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
